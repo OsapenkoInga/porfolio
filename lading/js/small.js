7da97b1adefd2df64a8a88e0cf34a97b29560ccb
OstapenkoInga <osapenko_inga88@mail.ru>
2017-02-15 00:19:28 +0200

    некоторые изменения

---
+++ lading/js/small.js
@@ -0,0 +1,8 @@
+﻿var w = $(window).width();
+if (w < 320) {
+    $('.slides').append('<li><img src="img/galerry_4.jpg" alt=""></li><li><img src="img/galerry_1.jpg" alt=""></li><li><img src="img/galerry_3.jpg" alt=""></li><li><img src="img/galerry_4.jpg" alt=""></li>')
+};
+
+$(window).resize(function () {
+    alert(w);
+});
